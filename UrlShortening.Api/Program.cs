1b2153c1b0f13da784d82ab32b3dde3a2149bbe7
mreshboboyev <mreshboboyev@gmail.com>
2024-12-27 20:08:33 +0500

    Building the UrlShortener API

--- UrlShortening.Api/Program.cs
+++ UrlShortening.Api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddHostedService<DatabaseInitializer>();
+builder.Services.AddScoped<UrlShorteningService>();
 
 var app = builder.Build();
 
@@ -16,6 +17,34 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/openapi/v1.json", "OpenAPI v1");
     });
 }
+
+app.MapPost("shorten", async (
+    string url,
+    UrlShorteningService urlShorteningService) =>
+{
+    if (!Uri.TryCreate(url, UriKind.Absolute, out _))
+    {
+        return Results.BadRequest("Invalid URL format");
+    }
+
+    var shortCode = await urlShorteningService.ShortenUrl(url);
+
+    return Results.Ok(new { shortCode });
+});
+
+app.MapGet("{shortCode}", async (
+    string shortCode,
+    UrlShorteningService urlShorteningService) =>
+{
+    var originalUrl = await urlShorteningService.GetOriginalUrl(shortCode);
+
+    return originalUrl is null ? Results.NotFound() : Results.Redirect(originalUrl);
+});
+
+app.MapGet("urls", async (UrlShorteningService urlShorteningService) =>
+{
+    return Results.Ok(await urlShorteningService.GetAllUrls());
+});
 
 app.UseHttpsRedirection();
 
---
+++ UrlShortening.Api/Services/UrlShorteningService.cs
@@ -0,0 +1,21 @@
+﻿using UrlShortening.Api.Models;
+
+namespace UrlShortening.Api.Services;
+
+internal sealed class UrlShorteningService
+{
+    public async Task<string> ShortenUrl(string originalUrl)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public async Task<string?> GetOriginalUrl(string shortCode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<ShortenedUrl>> GetAllUrls()
+    {
+        throw new NotImplementedException();
+    }
+}
